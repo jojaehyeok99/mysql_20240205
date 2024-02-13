e9352cc0878da1a0c11659bf62d6aed5a607537a
chojaehyeok99 <choco5366@naver.com>
2024-02-13 10:59:53 +0900

    2024-02-13 Transaction(2)

--- Transaction.sql
+++ Transaction.sql
@@ -2,6 +2,12 @@ use practice_sql;
 
 -- Transaction : SQL로 작업하는 작업 단위
 
+-- Transaction의 4가지 특성
+-- Atomicity (원자성) : 트랜잭션은 모두 성공하거나 모두 실패해야 한다.
+-- Consistency (일관성) : 트랜잭션의 작업 결과는 항상 일관성이 있어야 한다.
+-- Isolation (독립성) : 트랜잭션은 모두 독립적이어야 한다.
+-- Durablitiy (영구성) : 트랜잭션이 성공적으로 완료되면 영구히 반영되어야 한다.
+
 -- 회원가입 : USER - email_authentication
 -- 사용자는 아이디를 입력하고 중복확인을 함
 -- 만약 입력한 아이디가 중복된 아이디가 아니라면
@@ -37,9 +43,94 @@ INSERT email_authentication VALUES ('email@email.com', 'OCMD');
 
 -- 3. 이메일 인증 확인
 SELECT * FROM email_authentication 
-
 WHERE email = 'email@email.com' AND authentication_code = 'OCMD' ;
 
 -- 4. 회원가입 처리
 INSERT user VALUES ('idddd', 'P!ssw0rd', 'email@email.com');
+
+SHOW variables like '%commit%';
+SET AUTOCOMMIT = 0;
+
+# ------------------------------------------------------------------------------------ #
+-- 트랜잭션 시작
+START TRANSACTION;
+
+-- 사용자가 아이디를 'idddd2'로 입력했다.
+
+-- 1. 아이디 중복확인
+SELECT * FROM user WHERE id = 'idddd2';
+
+-- 사용자가 비밀번호를 'P!ssw0rd', 비밀번호 확인을 'P!ssw0rd',
+-- 이메일을 'email2@email.com', 서버가 생성한 코드는 'OCMD'이다.
+
+-- 2. 이메일 인증
+INSERT email_authentication VALUES ('email2@email.com', 'OCMD');
+
+-- 서버가 지정한 이메일로 인증 코드를 전송하고 사용자는 그 코드를 확인한 후 인증코드를 입력하여 인증함
+
+-- 3. 이메일 인증 확인
+SELECT * FROM email_authentication 
+WHERE email = 'email2@email.com' AND authentication_code = 'OCMD' ;
+
+-- 4. 회원가입 처리
+INSERT user VALUES ('idddd3', 'P!ssw0rd', 'email3@email.com');
+
+-- 트랜잭션 성공 처리 (영구히 적용)
+COMMIT;
+
+#----------------------------------------------------------------------------------#
+-- 트랜잭션 ROLLBACK 및 SAVEPOINT
+
+-- 사용자가 아이디를 'idddd3'로 입력했다.
+
+-- 1. 아이디 중복확인
+SELECT * FROM user WHERE id = 'idddd3';
+
+-- 사용자가 비밀번호를 'P!ssw0rd', 비밀번호 확인을 'P!ssw0rd',
+-- 이메일을 'email3@email.com', 서버가 생성한 코드는 'OCMD'이다.
+
+-- 2. 이메일 인증
+INSERT email_authentication VALUES ('email3@email.com', 'OCMD');
+
+-- 트랜잭션 초기상태로 변경 (트랜잭션 취소)
+ROLLBACK;
+
+-- 트랜잭션 롤백 위치 지정
+SAVEPOINT A;
+
+-- 서버가 지정한 이메일로 인증 코드를 전송하고 사용자는 그 코드를 확인한 후 인증코드를 입력하여 인증함
+
+-- 3. 이메일 인증 확인
+SELECT * FROM email_authentication 
+WHERE email = 'email3@email.com' AND authentication_code = 'OCMD' ;
+
+-- 4. 회원가입 처리
+INSERT user VALUES ('idddd3', 'P!ssw0rd', 'email3@email.com');
+
+-- 특정 세이브 포인트로 롤백
+ROLLBACK TO SAVEPOINT A;
+
+-- 트랜잭션 성공 처리 (영구히 적용)
+COMMIT;
+
+SELECT * FROM user;
+SELECT * FROM email_authentication;
+
+#------------------------------------------------------------------------------------#
+-- LIMIT 갯수 : 결과 테이블에서 지정한 갯수만큼의 상위 데이터만 보여줌
+SELECT * FROM jeju LIMIT 10;
+-- LIMIT 제외 레코드 갯수, 갯수 : 상위에서 제외 레코드 갯수만큼 제외 후 갯수만큼의 데이터만 보여줌
+SELECT * FROM jeju LIMIT 10, 3;
+
+
+
+
+
+
+
+
+
+
+
+
 
